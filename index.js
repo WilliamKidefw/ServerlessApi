a63bc9114587e4ed4d43b228ea0f13df88ac8224
WilliamKidefw <william_ryv_13@hotmail.com>
2020-06-08 11:58:07 -0500

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const bodyParser = require('body-parser');
 var sqlpool = require('./sqlpool')
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
